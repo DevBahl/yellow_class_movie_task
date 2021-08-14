6667c198087324f64618b9241480f4c269069869
DevBahl <devbahl12@yahoo.com>
2021-08-14 13:28:26 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/yellow_class_movie_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yellow_class_movie_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,12 @@
+const movies_data = [
+  {
+    "Name": "Interstellar",
+    "Director": "Christopher Nolan",
+    "Image": ""
+  },
+  {
+    "Name": "Conjuring",
+    "Director": "James Wan",
+    "Image": ""
+  }
+]
